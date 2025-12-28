48f56052b9e4f093a18688ee36580bc6dfe428b2
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2025-12-28 11:01:29 +0300

    Download exercise file and implement timeToMixJuice function
    Downloaded all the exercise files and implemented
    timeToMixJuice function to calculate the accurate
    time to prepare each drink.

---
+++ javascript/mixed-juices/mixed-juices.js
@@ -0,0 +1,52 @@
+// @ts-check
+//
+// The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion when
+// implementing this exercise.
+
+/**
+ * Determines how long it takes to prepare a certain juice.
+ *
+ * @param {string} name
+ * @returns {number} time in minutes
+ */
+export function timeToMixJuice(name) {
+  name = name.toLowerCase();
+  switch (name) {
+    case "pure strawberry joy":
+      return 0.5;
+    case "energizer":
+      return 1.5;
+    case "green garden":
+      return 1.5;
+    case "tropical island":
+      return 3;
+    case "all or nothing":
+      return 5;
+    default:
+      return 2.5;
+  }
+}
+
+/**
+ * Calculates the number of limes that need to be cut
+ * to reach a certain supply.
+ *
+ * @param {number} wedgesNeeded
+ * @param {string[]} limes
+ * @returns {number} number of limes cut
+ */
+export function limesToCut(wedgesNeeded, limes) {
+  throw new Error("Remove this line and implement the function");
+}
+
+/**
+ * Determines which juices still need to be prepared after the end of the shift.
+ *
+ * @param {number} timeLeft
+ * @param {string[]} orders
+ * @returns {string[]} remaining orders after the time is up
+ */
+export function remainingOrders(timeLeft, orders) {
+  throw new Error("Remove this line and implement the function");
+}
